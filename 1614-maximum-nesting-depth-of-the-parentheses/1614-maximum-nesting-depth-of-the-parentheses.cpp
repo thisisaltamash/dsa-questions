321adb6a42046bcf6cdcdf1ef8cb38e97379ec36
Altamash <29altamashansari@gmail.com>
2026-07-15 03:22:16 +0530

    Time: 0 ms (100%), Space: 8.4 MB (23.94%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0;
+        int maxDepth = 0;
+
+        for(char ch : s) {
+            if(ch == '(') {
+                depth++;
+                maxDepth = max(maxDepth, depth);
+            }
+            else if(ch == ')') {
+                depth--;
+            }
+        }
+
+        return maxDepth;
+    }
+};
